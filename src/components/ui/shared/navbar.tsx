c66b33819ce23797b3b139756828e1886ca99145
aguswesly <agustchannel@gmail.com>
2024-01-19 22:13:31 +0700

    feat : navigation add login register

--- src/components/ui/shared/navbar.tsx
+++ src/components/ui/shared/navbar.tsx
@@ -11,30 +11,26 @@ import { buttonVariants } from '../button'
 import { cn } from '@/lib/utils'
 
 import useScroll from '@/hooks/use-scroll'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 
 const MENU_ITEMS = [
   { label: 'For Business', href: '/for-business' },
-  { label: 'For Creators', href: '/for-creators' },
+  { label: 'For Creators', href: '/' },
   { label: 'Reach Beyond Borders', href: '/' },
   { label: 'Page Explore More', href: '/' },
 ] as const
 
 const MENU_ITEMS_DESKTOP = [
   { label: 'For Business', href: '/for-business' },
-  { label: 'For Creators', href: '/for-creators' },
-  { label: 'Schedule a Meeting', href: '/meet' },
-] as const
-
-const MENU_AUTH = [
-  { label: 'Login', href: '/meet' },
-  { label: 'Register', href: '/meet' },
+  { label: 'For Creators', href: '/' },
+  { label: 'Schedule a Meeting', href: '/' },
 ] as const
 
 type Props = {}
 
 export default function Navbar({}: Props) {
   const scrolled = useScroll(20)
+  const location = useLocation()
 
   return (
     <nav
@@ -61,11 +57,15 @@ export default function Navbar({}: Props) {
               <SheetHeader className="mt-6">
                 <SheetTitle className="text-2xl">MENU</SheetTitle>
               </SheetHeader>
-              <ul className="pt-10 px-4 flex flex-col gap-2 text-sm text-primary/80 font-semibold">
+              <div className="pt-10 px-4 flex flex-col gap-2 text-sm text-neutral-900/80">
                 {MENU_ITEMS.map((item) => (
                   <SheetClose asChild>
                     <NavLink
-                      className="py-2 border-b"
+                      className={`py-2 border-b ${
+                        location.pathname === item.href
+                          ? 'text-black font-semibold'
+                          : ''
+                      }`}
                       key={item.label}
                       to={item.href}
                     >
@@ -73,8 +73,25 @@ export default function Navbar({}: Props) {
                     </NavLink>
                   </SheetClose>
                 ))}
-                <li></li>
-              </ul>
+
+                <SheetClose asChild>
+                  <NavLink
+                    className="py-2 mt-5 text-center text-primary font-bold"
+                    to={'/login'}
+                  >
+                    Login
+                  </NavLink>
+                </SheetClose>
+
+                <SheetClose asChild>
+                  <NavLink
+                    className="py-2 text-center font-semibold bg-primary text-background rounded-full"
+                    to={'/register'}
+                  >
+                    Register
+                  </NavLink>
+                </SheetClose>
+              </div>
             </SheetContent>
           </Sheet>
         </div>
