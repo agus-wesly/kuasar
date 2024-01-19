f994be3cc8df0f911a00db0601802e8898165c63
aguswesly <agustchannel@gmail.com>
2024-01-19 12:15:53 +0700

    fix : video size

--- src/components/home/hall-of-fame.tsx
+++ src/components/home/hall-of-fame.tsx
@@ -69,7 +69,7 @@ function CardARCreation(
   props: Pick<Project, 'created_by' | 'video' | 'title'>
 ) {
   return (
-    <div className="w-full rounded-lg overflow-hidden shadow-md relative">
+    <div className="w-full max-w-60 md:max-w-none rounded-lg overflow-hidden shadow-md relative">
       <div className="flex w-full bg-purple-100 aspect-[9/16] items-center justify-center p-0">
         {/* PLAYER HERE */}
         <video
