2c97c75a9a64b317c7a43572da16206e46ff37af
aguswesly <agustchannel@gmail.com>
2024-01-22 11:53:51 +0700

    feat : add toggle to input password

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,17 +1,22 @@
-import * as React from "react"
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
+import { Eye, EyeOff } from 'lucide-react'
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    if (type === 'password') {
+      return <InputPassword />
+    }
+
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
@@ -20,6 +25,40 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     )
   }
 )
-Input.displayName = "Input"
+
+const InputPassword = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    const [showInput, setShowInput] = React.useState(false)
+
+    const EyeIcon = showInput ? Eye : EyeOff
+
+    function handleToggleInput() {
+      setShowInput((prev) => !prev)
+    }
+
+    return (
+      <div className="relative">
+        <input
+          type={showInput ? 'text' : 'password'}
+          className={cn(
+            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+
+        <button
+          type="button"
+          onClick={handleToggleInput}
+          className="absolute right-3 top-3 w-4 h-4"
+        >
+          <EyeIcon className="size-full" />
+        </button>
+      </div>
+    )
+  }
+)
+Input.displayName = 'Input'
 
 export { Input }
