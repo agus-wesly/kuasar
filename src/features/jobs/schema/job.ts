6d63352e70700cb231dcdf0c5d463614b75ff62c
aguswesly <agustchannel@gmail.com>
2024-02-07 11:57:29 +0700

    feat : add job functionality

--- src/features/jobs/schema/job.ts
+++ src/features/jobs/schema/job.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 
-export const jobSchema = z.object({
+export const jobCreateSchema = z.object({
   title: z.string().min(2).max(200),
   description: z.string(),
-  deadline: z.date(),
-  type_id: z.number(),
+  deadline: z.string().min(1, 'Deadline is required!'),
+  type_id: z.number({ required_error: 'Job type is required!' }),
 })
--- src/pages/dashboard/dashboard-admin/jobs/create/index.tsx
+++ src/pages/dashboard/dashboard-admin/jobs/create/index.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button'
-import { Calendar } from '@/components/ui/calendar'
 import { Input } from '@/components/ui/input'
 import {
   Select,
@@ -8,21 +7,26 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import { Skeleton } from '@/components/ui/skeleton'
 import { Textarea } from '@/components/ui/textarea'
-import { jobSchema } from '@/features/jobs/schema/job'
+import { useAccessToken } from '@/features/auth/hooks/use-auth'
+import { useJobTypesQuery } from '@/features/jobs/query'
+import { jobCreateSchema } from '@/features/jobs/schema/job'
 import { axios } from '@/plugin/axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { AxiosError } from 'axios'
+import { Loader2 } from 'lucide-react'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 type Props = {}
 
 export default function DashboardJobCreatePage({}: Props) {
-  const { form, handleRegister, isSubmitting, errors } = useCreateNewJob()
-
-  const [date, setDate] = useState<Date | undefined>(new Date())
+  const { form, handleCreateNewJob, isSubmitting, errors } = useCreateNewJob()
+  const { data, isLoading: isLoadingJobType } = useJobTypesQuery()
+  const jobTypes = data?.data ?? []
 
   return (
     <div className="w-full">
@@ -38,7 +42,7 @@ export default function DashboardJobCreatePage({}: Props) {
       </div>
 
       <div className="p-5 rounded-xl shadow-sm mb-5">
-        <form onSubmit={handleRegister} className="w-full max-w-lg my-5">
+        <form onSubmit={handleCreateNewJob} className="w-full max-w-lg my-5">
           <fieldset disabled={isSubmitting} className="grid gap-4 text-sm">
             <div className="grid gap-1">
               <label htmlFor="title">Title</label>
@@ -60,6 +64,7 @@ export default function DashboardJobCreatePage({}: Props) {
               <Textarea
                 id="description"
                 placeholder="Job description..."
+                required
                 {...form.register('description')}
               />
               {errors.description && (
@@ -71,11 +76,12 @@ export default function DashboardJobCreatePage({}: Props) {
 
             <div className="grid gap-1">
               <label htmlFor="deadline">Deadline</label>
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                className="rounded-md border shadow"
+              <Input
+                id="title"
+                placeholder="Job deadline..."
+                type="date"
+                min={transformDateToYMD(new Date())}
+                {...form.register('deadline')}
               />
 
               {errors.deadline && (
@@ -86,16 +92,28 @@ export default function DashboardJobCreatePage({}: Props) {
             </div>
 
             <div className="grid gap-1">
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Job Type"></SelectValue>
-                </SelectTrigger>
+              {isLoadingJobType ? (
+                <Skeleton className="h-8" />
+              ) : (
+                <>
+                  <label htmlFor="jobtype">Job Type</label>
+                  <Select
+                    onValueChange={(e) => form.setValue('type_id', Number(e))}
+                  >
+                    <SelectTrigger id="jobtype">
+                      <SelectValue placeholder="Select job type"></SelectValue>
+                    </SelectTrigger>
 
-                <SelectContent {...form.register('type_id')} id="arMonetize">
-                  <SelectItem value="true">Yes</SelectItem>
-                  <SelectItem value="false">No</SelectItem>
-                </SelectContent>
-              </Select>
+                    <SelectContent>
+                      {jobTypes.map((item) => (
+                        <SelectItem key={item.id} value={String(item.id)}>
+                          {item.type}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </>
+              )}
               {errors.type_id && (
                 <p className="text-xs text-destructive">
                   {errors.type_id.message}
@@ -104,7 +122,9 @@ export default function DashboardJobCreatePage({}: Props) {
             </div>
 
             <Button className=" px-8">
-              {/* {isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />} */}
+              {isSubmitting && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
               Create Job
             </Button>
           </fieldset>
@@ -115,8 +135,8 @@ export default function DashboardJobCreatePage({}: Props) {
 }
 
 function useCreateNewJob() {
-  const form = useForm<z.infer<typeof jobSchema>>({
-    resolver: zodResolver(jobSchema),
+  const form = useForm<z.infer<typeof jobCreateSchema>>({
+    resolver: zodResolver(jobCreateSchema),
   })
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [errorCreateNewApplication, setErrorCreateNewApplication] =
@@ -124,13 +144,26 @@ function useCreateNewJob() {
       message: string
       statusCode: number
     }> | null>(null)
+
+  const accessToken = useAccessToken((state) => state.accessToken)
   // const navigate = useNavigate()
 
-  const handleRegister = form.handleSubmit(async (data) => {
+  const handleCreateNewJob = form.handleSubmit(async (data) => {
     try {
       setIsSubmitting(true)
-      await axios.post('/applications/create', {
-        ...data,
+      await axios.post(
+        '/jobs/create',
+        {
+          ...data,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      toast('Successfully created  ✅', {
+        description: 'New job has been created !',
       })
     } catch (error) {
       if (error instanceof AxiosError) {
@@ -144,10 +177,19 @@ function useCreateNewJob() {
   const errors = form.formState.errors
 
   return {
-    handleRegister,
+    handleCreateNewJob,
     isSubmitting,
     errors,
     form,
     errorCreateNewApplication,
   }
+}
+
+function transformDateToYMD(date: Date) {
+  const year = date.getFullYear()
+  let month: string | number = date.getMonth() + 1
+  month = month < 10 ? '0' + month : month
+  let day: string | number = date.getDate()
+  day = day < 10 ? '0' + day : day
+  return `${year}-${month}-${day}`
 }
