96924ee72c5147a666a9f1b4d2571d65b33cf133
aguswesly <agustchannel@gmail.com>
2024-02-11 16:32:02 +0700

    refactor : separate router

---
+++ src/routeTree.tsx
@@ -0,0 +1,135 @@
+import LoginPage from './pages/login'
+import RegisterPage from './pages/register'
+import HomePage from './pages/home'
+import RequireUnAuth from './layouts/require-unauth'
+import RequireAuth from './layouts/require-auth'
+import VerifyAccount, {
+  loader as VerifyAccountLoader,
+} from './pages/verify-account'
+import NotFoundPage from './pages/not-found'
+import DashboardPageLayout from './layouts/dashboard-layout'
+import LandingPageLayout from './layouts/landing-page-layout'
+
+import DashboardPage, {
+  DashboardApplicationsPage,
+  DashboardIndexPage,
+  DashboardJobsPage,
+  DashboardProjectsPage,
+} from './pages/dashboard'
+import DashboardAdminApplicationCreate from './pages/dashboard/dashboard-admin/applications/create'
+import DashboardJobCreatePage from './pages/dashboard/dashboard-admin/jobs/create'
+import DashboardJobUpdatePage from './pages/dashboard/dashboard-admin/jobs/update'
+
+import RootLayout from './pages/root'
+import { createBrowserRouter } from 'react-router-dom'
+import { Suspense, lazy } from 'react'
+
+const ExploreMorePage = lazy(() => import('./pages/explore-more'))
+const ForBusinessPage = lazy(() => import('./pages/for-business'))
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        element: <LandingPageLayout />,
+        children: [
+          {
+            index: true,
+            element: <HomePage />,
+          },
+          {
+            path: '/for-business',
+            element: (
+              <Suspense fallback={null}>
+                <ForBusinessPage />
+              </Suspense>
+            ),
+          },
+          {
+            path: '/explore-more',
+            element: (
+              <Suspense fallback={null}>
+                <ExploreMorePage />
+              </Suspense>
+            ),
+          },
+          {
+            element: <RequireUnAuth />,
+            children: [
+              {
+                path: '/login',
+                element: <LoginPage />,
+              },
+              {
+                path: '/register',
+                element: <RegisterPage />,
+              },
+              {
+                path: '/verify-account',
+                element: <VerifyAccount />,
+                loader: VerifyAccountLoader,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        element: <RequireAuth />,
+        children: [
+          {
+            element: <DashboardPageLayout />,
+            children: [
+              {
+                path: '/dashboard',
+                element: <DashboardPage />,
+                children: [
+                  {
+                    index: true,
+                    element: <DashboardIndexPage />,
+                  },
+                  {
+                    path: 'projects',
+                    element: <DashboardProjectsPage />,
+                  },
+                  {
+                    path: 'applications',
+                    children: [
+                      { index: true, element: <DashboardApplicationsPage /> },
+                      {
+                        path: 'create',
+                        element: <DashboardAdminApplicationCreate />,
+                      },
+                    ],
+                  },
+                  {
+                    path: 'jobs',
+                    children: [
+                      {
+                        index: true,
+                        element: <DashboardJobsPage />,
+                      },
+                      {
+                        path: 'create',
+                        element: <DashboardJobCreatePage />,
+                      },
+                      {
+                        path: 'update/:id',
+                        element: <DashboardJobUpdatePage />,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+])
