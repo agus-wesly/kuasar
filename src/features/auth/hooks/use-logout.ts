631b22ce0cb598e61a5b34fd7aef3b4ed3783c38
aguswesly <agustchannel@gmail.com>
2024-03-09 14:13:33 +0700

    fix : clear all cached data when logout

--- src/features/auth/hooks/use-logout.ts
+++ src/features/auth/hooks/use-logout.ts
@@ -1,17 +1,20 @@
 import { useShallow } from 'zustand/react/shallow'
 import { useAccessToken, useUser } from './use-auth'
 import { useNavigate } from 'react-router-dom'
+import { useQueryClient } from '@tanstack/react-query'
 
 export function useLogout() {
   const setUser = useUser(useShallow((state) => state.setUser))
   const setAccessToken = useAccessToken(
-    useShallow((state) => state.setAccessToken)
+    useShallow((state) => state.setAccessToken),
   )
   const navigate = useNavigate()
+  const queryClient = useQueryClient()
 
   function logOut() {
     setUser(null)
     setAccessToken(null)
+    queryClient.clear()
     navigate('/')
   }
 
